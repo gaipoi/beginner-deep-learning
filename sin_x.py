f4015be063263deda89495ebce22be06613b36a3
s <s>
2022-10-15 00:15:11 +0900

    Add sample sin_x

---
+++ sin_x.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def showGraph():
+    x = np.arange(0, 6, 0.1)
+    y = np.sin(x)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+    plt.plot(x, y1, label="sin")
+    plt.plot(x, y2, linestyle = "--", label="cos")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.show()
+
+
+showGraph()
